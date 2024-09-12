b3fa8510a579f96d1cc4a726fd831775bb60b611
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-12 18:33:57 +0530

    button changes

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -7,7 +7,7 @@ import clsx from "clsx";
 import { cn } from "../../lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -23,9 +23,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-3 py-2",
-        sm: "h-7 rounded-md px-2",
-        lg: "h-11 rounded-md px-8",
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded px-3",
+        lg: "h-11 rounded px-8",
         icon: "h-9 w-9",
       },
     },
@@ -58,7 +58,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
     {
       className,
-      variant,
+      variant = "primary",
       size,
       isLoading,
       children,
